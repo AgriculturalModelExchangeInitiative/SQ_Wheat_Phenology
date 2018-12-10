5fd122298243950b219a8c187bdd0edf10a270fb
cyrillemidingoyi <34443570+cyrillemidingoyi@users.noreply.github.com>
2018-12-10 16:27:50 +0100

    Update cumulttfrom.f90

--- test/f90/cumulttfrom.f90
+++ test/f90/cumulttfrom.f90
@@ -2,19 +2,21 @@
 program phenology
 
     integer::SwitchMaize=0
+    real:: cumulTT=120
     real,dimension (2)::calendarCumuls=(20)
     character(len=100),dimension(2):: calendarMoments = ("Anthesis")
     real:: cumulTTFromZC_65, cumulTTFromZC_39, cumulTTFromZC_91
 
-    call CumulFROM(calendarMoments, calendarCumuls, SwitchMaize,cumulTTFromZC_65, cumulTTFromZC_39, cumulTTFromZC_91)
+    call CumulFROM(cumulTT, calendarMoments, calendarCumuls, SwitchMaize,cumulTTFromZC_65, cumulTTFromZC_39, cumulTTFromZC_91)
 
 contains
 
-    SUBROUTINE CumulFROM( calendarMoments, calendarCumuls, SwitchMaize,cumulTTFromZC_65, cumulTTFromZC_39, cumulTTFromZC_91) ! iutput
+    SUBROUTINE CumulFROM( cumulTT, calendarMoments, calendarCumuls, SwitchMaize,cumulTTFromZC_65, cumulTTFromZC_39, cumulTTFromZC_91) ! iutput
 
 
     !     INPUT VARIABLES:
-    INTEGER SwitchMaize
+    integer SwitchMaize
+    real cumulTT
     real,dimension (:):: calendarCumuls
     character,dimension(:):: calendarMoments
     integer indice
@@ -29,22 +31,22 @@ contains
     IF(ANY(calendarMoments=="Anthesis")) THEN
         IF (SwitchMaize==0) THEN
             indice = int(FINDLOC(calendarMoments,"Anthesis",1))
-            cumulTTFromZC_65 = calendarCumuls(int(FINDLOC(calendarMoments,"Anthesis",1)))
+            cumulTTFromZC_65 = cumulTT - calendarCumuls(int(FINDLOC(calendarMoments,"Anthesis",1)))
             print *, cumulTTFromZC_65
         END IF
     END IF
 
     IF(ANY(calendarMoments=="FlagLeafLiguleJustVisible")) THEN
         IF (SwitchMaize==0) THEN
-            cumulTTFromZC_39 = calendarCumuls(int(FINDLOC(calendarMoments,"FlagLeafLiguleJustVisible",1)))
+            cumulTTFromZC_39 = cumulTT - calendarCumuls(int(FINDLOC(calendarMoments,"FlagLeafLiguleJustVisible",1)))
             print *, cumulTTFromZC_39
         END IF
     END IF
 
     IF(ANY(calendarMoments=="FlagLeafLiguleJustVisible")) THEN
         IF (SwitchMaize==0) THEN
-            cumulTTFromZC_91 = calendarCumuls(int(FINDLOC(calendarMoments,"FlagLeafLiguleJustVisible",1)))
-            print *, umulTTFromZC_91
+            cumulTTFromZC_91 = cumulTT - calendarCumuls(int(FINDLOC(calendarMoments,"FlagLeafLiguleJustVisible",1)))
+            print *, cumulTTFromZC_91
         END IF
     END IF
     RETURN
