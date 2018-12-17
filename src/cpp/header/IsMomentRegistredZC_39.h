a40b120287571f10c40ae1170e7439b7619d72c4
cyrillemidingoyi <34443570+cyrillemidingoyi@users.noreply.github.com>
2018-12-17 17:27:20 +0100

    Update IsMomentRegistredZC_39.h

--- src/cpp/header/IsMomentRegistredZC_39.h
+++ src/cpp/header/IsMomentRegistredZC_39.h
@@ -1,8 +1,6 @@
 # include<vector>
 struct IsMomentRegistredZC_39 {
     int isMomentRegistredZC_39;
-
 };
-
 using namespace std;
 IsMomentRegistredZC_39 Calculate_IsMomentRegistredZC_39(vector<string> calendarMoments);
